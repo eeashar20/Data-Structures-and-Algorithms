be937fd51dd4a16aab536094565b7b80c94cd8a9
asharharoon <ashar_haroon2006@yahoo.com>
2021-06-14 00:11:27 +0500

    Add function to insert node at the beginning and at the end of a LinkedList.

--- LinkedList_Class/LinkedList.py
+++ LinkedList_Class/LinkedList.py
@@ -1,7 +1,29 @@
-import Node
+from Node import Node
 
 
 class LinkedList:
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.first: Node = None
+        self.last: Node = None
+
+    """This function inserts the node at the beginning of the LinkedList"""
+    def add_first(self, data):
+        if self.first is None:
+            self.first = Node(data)
+            self.last = self.first
+            return
+        else:
+            node: Node = self.first
+            self.first = Node(data)
+            self.first.next = node
+
+    """This function inserts the node at the beginning of the LinkedList"""
+    def add_last(self, data):
+        if self.first is None:
+            self.first = Node(data)
+            self.last = self.first
+            return
+        else:
+            node: Node = self.last
+            self.last = Node(data)
+            node.next = self.last
---
+++ LinkedList_Class/main.py
@@ -0,0 +1,12 @@
+from LinkedList import LinkedList
+from Node import Node
+
+llist = LinkedList()
+llist.add_first(4)
+llist.add_first(7)
+llist.add_first(9)
+llist.add_first(9)
+llist.add_last(12)
+print(llist.first.value)
+print(llist.last.value)
+
